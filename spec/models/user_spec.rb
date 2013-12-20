6fecc8c5c2780011df033ad6ca789621275f40c7
Kenny Nguyen <knguyeniii@gmail.com>
2013-12-20 17:52:59 +0700

    still on ch9 - modal data review

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,9 +16,18 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
-#  it { should be_valid }
-#  it { should respond_to(:authenticate) }
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -92,3 +101,4 @@ private
     its(:remember_token) { should_not be_blank }
   end
 end  
+end
