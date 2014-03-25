37ea7f4a3a81d1226956db0ed689294e8445bc4e
Kenny Nguyen <knguyeniii@gmail.com>
2014-03-24 19:21:38 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -86,19 +86,18 @@ describe "Authentication" do
 #          it { should have_title('Sign in') }
 #        end
 
-#        describe "visiting the following page" do
-#          before { visit following_user_path(user) }
-#          it { should have_title('Sign in') }
-#        end
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
 
-#        describe "visiting the followers page" do
-#          before { visit followers_user_path(user) }
-#          it { should have_title('Sign in') }
-#        end
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "in the Microposts controller" do
-
         describe "submitting to the create action" do
           before { post microposts_path }
           specify { expect(response).to redirect_to(signin_path) }
@@ -110,17 +109,17 @@ describe "Authentication" do
         end
       end
 
-#      describe "in the Relationships controller" do
-#        describe "submitting to the create action" do
-#          before { post relationships_path }
-#          specify { expect(response).to redirect_to(signin_path) }
-#        end
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
 
-#        describe "submitting to the destroy action" do
-#          before { delete relationship_path(1) }
-#          specify { expect(response).to redirect_to(signin_path) }
-#        end
-#      end
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
 
     describe "as wrong user" do
